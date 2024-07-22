0f6acc331c289ba90b07a9313cfc1778803712fe
sidharth <sidguleria0@gmail.com>
2024-07-22 22:12:12 +0530

    added component

---
+++ resources/views/admin/manage-itservices.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layout.layout')
+@section('content')
+<div class="p-4 border-2 border-dashed rounded-lg border-gray-700 font-Robotomedium underline">
+  <!-- resources/views/admin/admin.blade.php -->
+@include('admin.components.heading', ['headingText' => 'Add And Delete It Services Here', 'headingClass' => 'text-2xl font-semibold font-Montserrat text-gray-950'])
+
+</div>
+
+@stop
