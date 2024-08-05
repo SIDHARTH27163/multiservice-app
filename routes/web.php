ba051edcbf595dfe4e4a1761b72b848b69b02995
sidharth <sidguleria0@gmail.com>
2024-08-05 22:43:29 +0530

    case study part added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ITServiceController;
+use  App\Http\controllers\ITCaseStudiesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,5 +12,5 @@ Route::view('/admin', 'admin.admin');
 // Resource routes for ITServiceController
 // This will create routes for index, create, store, show, edit, update, and destroy methods
 Route::resource('admin/manageitservices', ITServiceController::class)->except(['show']);
-
+Route::resource('admin/managecasestudies', ITCaseStudiesController::class)->except(['show']);
 /*admin routes ends */
